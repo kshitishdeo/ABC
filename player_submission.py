eaf4ec2aa8c7b604d2e0e02819da8182c0aa9d8e
root <root@localhost.localdomain>
2017-01-16 15:15:32 -0500

    major changes

--- player_submission.py
+++ player_submission.py
@@ -9,15 +9,21 @@
 class OpenMoveEvalFn():
     """Evaluation function that outputs a 
     score equal to how many moves are open
-    for your computer player on the board."""
+    for your computer player on the board 
+    minus the moves open for opponent player."""
     def score(self, game, maximizing_player_turn=True):
-        a=0
-        dict1 = game.get_legal_moves()
-        for i in dict1:
-            for j in dict1[i]:
-                a=a+1;
-        dict2= game.get_opponent_moves()
-        return a-len(dict2)
+        # TODO: finish this function!
+        return eval_func
+        
+# Submission Class 2
+class CustomEvalFn():
+    """Custom evaluation function that acts
+    however you think it should. This is not
+    required but highly encouraged if you
+    want to build the best AI possible."""
+    def score(self, game, maximizing_player_turn=True):
+        # TODO: finish this function!
+        return eval_func
 
 class CustomPlayer():
     # TODO: finish this class!
@@ -28,120 +34,36 @@ class CustomPlayer():
     You must finish and test this player
     to make sure it properly uses minimax
     and alpha-beta to return a good move
-    in less than 1000 milliseconds."""
+    in less than 5 seconds."""
     def __init__(self,  search_depth=3, eval_fn=OpenMoveEvalFn()):
         # if you find yourself with a superior eval function, update the
         # default value of `eval_fn` to `CustomEvalFn()`
         self.eval_fn = eval_fn
         self.search_depth = search_depth
-        
-    def opponent_eval(self,game):
-        a=0
-        dict1 = game.get_legal_moves()
-        for i in dict1:
-            for j in dict1[i]:
-                a=a+1;
-        dict2= game.get_opponent_moves()
-        return len(dict2)-a
+
     
     def move(self, game, legal_moves, time_left):
-        best_move,best_queen, utility = self.alphabeta(game,time_left, depth=self.search_depth) # queen added as return type
-      
-        return best_move, best_queen # queen added as return type
+        best_move,best_queen, utility = self.minimax(game,time_left, depth=self.search_depth)   
+        #change minimax to alphabeta after completing alphabeta part of assignment 
+        return best_move, best_queen 
 
 
-    def utility(self, game, maximizing):
-        legal_moves=game.get_legal_moves()
-        if maximizing:
-            if not game.get_opponent_moves():
-                return float("inf")
-            if (not len(legal_moves[game.__active_players_queen1__]) and not len(legal_moves[game.__active_players_queen2__])):
-                return float("-inf")
+    def utility(self, game):
+        """TODO: Update this function to calculate the utility of a game state"""
+        
+        if game.is_winner(self):
+            return float("inf")
 
-            return self.eval_fn.score(game)
+        if game.is_opponent_winner(self):
+            return float("-inf")
 
-        else:
-            if (not len(legal_moves[game.__active_players_queen1__]) and not len(legal_moves[game.__active_players_queen2__])):
-                return float("inf")
-            if not game.get_opponent_moves():
-                return float("-inf")
-
-            return self.opponent_eval(game)
+        return self.eval_fn.score(game)
 
 
     def minimax(self, game, time_left, depth=float("inf"), maximizing_player=True):
-        legal_moves = game.get_legal_moves()
-        if not depth or (not len(legal_moves[game.__active_players_queen1__]) and not len(legal_moves[game.__active_players_queen2__])): #to change legal move
-            return None, None, self.utility(game, maximizing_player)
-
-        if maximizing_player:
-            best_move = None
-            best_queen = None
-            best_val =  float("-inf")
-            for queen in legal_moves:
-                for move in legal_moves[queen]:
-                    _,_, val = self.minimax(game.forecast_move(move,queen), time_left, depth -1, False )
-                    if val > best_val:
-                        best_val = val
-                        best_move = move
-                        best_queen = queen
-
-        else:
-            best_move = None
-            best_queen = None
-            best_val = float("inf")
-            for queen in legal_moves:
-                for move in legal_moves[queen]:
-                    _,_, val = self.minimax(game.forecast_move(move,queen), time_left, depth -1, True)
-                    if val < best_val:
-                        best_val = val
-                        best_move = move
-                        best_queen = queen
-
+        # TODO: finish this function!
         return best_move,best_queen, best_val
 
     def alphabeta(self, game, time_left, depth=float("inf"), alpha=float("-inf"), beta=float("inf"), maximizing_player=True):
-        legal_moves = game.get_legal_moves()
-
-        if not depth or (not len(legal_moves[game.__active_players_queen1__]) and not len(legal_moves[game.__active_players_queen2__])): #to change legal move
-            return None, None, self.utility(game, maximizing_player)
-
-        if maximizing_player:
-            val = float("-inf")
-            best_move = None
-            best_queen = None
-            for queen in legal_moves:
-                for move in legal_moves[queen]:
-                    node = game.forecast_move(move,queen)
-                    _,_, new_val = self.alphabeta(node, time_left, depth-1, alpha, beta, False)
-
-                    if new_val > val:
-                        val = new_val
-                        best_move = move
-                        best_queen = queen
-                    alpha = max( alpha, val)
-
-                    if beta <= alpha:
-                        return best_move,best_queen, beta
-            return best_move,best_queen, val
-
-        else:
-            val = float("inf")
-            best_move = None
-            best_queen = None
-            for queen in legal_moves:
-                for move in legal_moves[queen]:
-                    node = game.forecast_move(move,queen)
-                    _,_, new_val = self.alphabeta(node, time_left, depth -1 , alpha, beta, True)
-
-                    if new_val < val:
-                        val = new_val
-                        best_move = move
-                        best_queen = queen
-                    beta = min(beta, val)
-
-                    if beta <= alpha:
-                        return best_move,best_queen, alpha
-
-            return best_move, best_queen, val
-
+        # TODO: finish this function!
+        return best_move, best_queen, val
